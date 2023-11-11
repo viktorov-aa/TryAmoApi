45156c5dfcee1085c4806beb5eafef8eef399cb9
Andrey.Viktorov <Andrey.Viktorov@Renlife.com>
2023-11-11 15:36:51 +0400

    feat(config): примитивный конфиг

---
+++ src/config/config.py
@@ -0,0 +1,35 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+from dataclasses_json import DataClassJsonMixin
+from yamldataclassconfig import create_file_path_field
+from yamldataclassconfig.config import YamlDataClassConfig
+
+
+@dataclass
+class AmoConfig(DataClassJsonMixin):
+    integration_id: str = None
+    secret_key: str = None
+    base_url: str = None
+
+
+@dataclass
+class DatabaseConfig(DataClassJsonMixin):
+    path: str = None
+
+
+@dataclass
+class Config(YamlDataClassConfig):
+    amo: AmoConfig = None
+    database: DatabaseConfig = None
+
+    FILE_PATH: Path = create_file_path_field(Path(__file__).parent / 'config.yaml')
+
+
+if __name__ == '__main__':
+    config = Config()
+    config.load()
+    print(config.amo.integration_id)
+    print(config.amo.secret_key)
+    print(config.amo.base_url)
+    print(config.database.path)
